4d3089369435e9fc269bb104fc3b0a39cd16f82d
Aidan T. Manning <periodicaidan@gmail.com>
2020-10-15 21:40:51 -0500

    added validation for the email address input

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 const LICENSES = ['GPLv3', 'MIT', 'Apache', 'BSD-3-Clause', 'BSD-2-Clause', 'LGPL', 'MPL', 'CDDL', 'EPL', 'CC0']
 
+// RFC 5322-compliant regular expression that matches an email address
+// Don't bother
+const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
 // array of questions for user
 const questions = [
     {
@@ -43,6 +47,7 @@ const questions = [
     {
         name: 'email',
         message: 'Email:',
+        validate: email => EMAIL_REGEX.test(email),
     }
 ];
 
